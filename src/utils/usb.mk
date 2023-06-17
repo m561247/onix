859a3afff47a56a6e3a1a161f9460258c63bb66b
Steven Kang <kangweibaby@163.com>
2023-06-17 12:54:42 +0800

    update usb

--- src/utils/usb.mk
+++ src/utils/usb.mk
@@ -1,14 +1,7 @@
-USB_BIN:= 
 USB_DEV:= /dev/sdb
 
 .PHONY: usb
 usb: $(USB_DEV) $(BUILD)/master.img
-	sudo dd if=/dev/sdb of=$(BUILD)/usb.bin bs=512 count=1 conv=notrunc
-	sudo chown $(USER) $(BUILD)/usb.bin
 
-	dd if=$(BUILD)/master.img of=$(BUILD)/usb.bin bs=446 count=1 conv=notrunc
-
-	cp $(BUILD)/master.img $(BUILD)/usb.img
-	dd if=$(BUILD)/usb.bin of=$(BUILD)/usb.img bs=512 count=1 conv=notrunc
-
-	sudo dd if=$(BUILD)/usb.img of=/dev/sdb bs=1 count=$$(stat -c%s "$(BUILD)/master.img") conv=notrunc
+# 将 master 的 16M 写入 U 盘
+	sudo dd if=$(BUILD)/master.img of=/dev/sdb bs=1024 count=16384 conv=notrunc
